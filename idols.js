2cf62b90d3c1d49d54b3c7994b241fa1d4e9f7aa
ahelloprojectfan <63974154+ahelloprojectfan@users.noreply.github.com>
2021-06-29 11:36:43 -0500

    Add files via upload

---
+++ idols.js
@@ -0,0 +1,488 @@
+let idols = [
+    {
+      "name": "譜久村聖",
+      "img": "https://i.imgur.com/UBZr81a.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "生田衣梨奈",
+      "img": "https://i.imgur.com/lwj15Jh.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "石田亜佑美",
+      "img": "https://i.imgur.com/cX6QWYd.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "佐藤優樹",
+      "img": "https://i.imgur.com/bY9Omj7.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "小田さくら",
+      "img": "https://i.imgur.com/HbEt5Xr.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "野中美希",
+      "img": "https://i.imgur.com/M1VZNZp.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "牧野真莉愛",
+      "img": "https://i.imgur.com/NrmkbUu.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "羽賀朱音",
+      "img": "https://i.imgur.com/FZaWtfv.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "加賀楓",
+      "img": "https://i.imgur.com/1MwJCpC.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "横山玲奈",
+      "img": "https://i.imgur.com/a6vd9kS.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "森戸知沙希",
+      "img": "https://i.imgur.com/kf8MpP0.jpg",
+      "group": [
+        "mm",
+        "cg"
+      ]
+    },
+    {
+      "name": "北川莉央",
+      "img": "https://i.imgur.com/YYo8oLc.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "岡村ほまれ",
+      "img": "https://i.imgur.com/lfgOlHg.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "山﨑愛生",
+      "img": "https://i.imgur.com/V4Y2PJk.jpg",
+      "group": [
+        "mm"
+      ]
+    },
+    {
+      "name": "竹内朱莉",
+      "img": "https://i.imgur.com/7No77GR.jpg",
+      "group": [
+        "angerme"
+      ]
+    },
+    {
+      "name": "佐々木莉佳子",
+      "img": "https://i.imgur.com/MsNZ8Ag.jpg",
+      "group": [
+        "angerme"
+      ]
+    },
+    {
+      "name": "上國料萌衣",
+      "img": "https://i.imgur.com/r4IYt1p.jpg",
+      "group": [
+        "angerme"
+      ]
+    },
+    {
+      "name": "笠原桃奈",
+      "img": "https://i.imgur.com/kyqxxLw.jpg",
+      "group": [
+        "angerme"
+      ]
+    },
+    {
+      "name": "川村文乃",
+      "img": "https://i.imgur.com/nzYTylo.jpg",
+      "group": [
+        "angerme"
+      ]
+    },
+    {
+      "name": "伊勢鈴蘭",
+      "img": "https://i.imgur.com/cyIMXKe.jpg",
+      "group": [
+        "angerme"
+      ]
+    },
+    {
+      "name": "橋迫鈴",
+      "img": "https://i.imgur.com/4JzqvRE.jpg",
+      "group": [
+        "angerme"
+      ]
+    },
+    {
+      "name": "為永幸音",
+      "img": "https://i.imgur.com/4rGKBwU.jpg",
+      "group": [
+        "angerme"
+      ]
+    },
+    {
+      "name": "川名凜",
+      "img": "https://i.imgur.com/PUYfGll.jpg",
+      "group": [
+        "angerme"
+      ]
+    },
+    {
+      "name": "松本わかな",
+      "img": "https://i.imgur.com/3h6IdIu.jpg",
+      "group": [
+        "angerme"
+      ]
+    },
+    {
+      "name": "金澤朋子",
+      "img": "https://i.imgur.com/9SreQGU.jpg",
+      "group": [
+        "jj"
+      ]
+    },
+    {
+      "name": "植村あかり",
+      "img": "https://i.imgur.com/cnn5SY0.jpg",
+      "group": [
+        "jj"
+      ]
+    },
+    {
+      "name": "稲場愛香",
+      "img": "https://i.imgur.com/HPp9U8z.jpg",
+      "group": [
+        "jj",
+        "cg"
+      ]
+    },
+    {
+      "name": "井上玲音",
+      "img": "https://i.imgur.com/smedCU7.jpg",
+      "group": [
+        "jj",
+        "kf"
+      ]
+    },
+    {
+      "name": "段原瑠々",
+      "img": "https://i.imgur.com/RBtM83x.jpg",
+      "group": [
+        "jj"
+      ]
+    },
+    {
+      "name": "工藤由愛",
+      "img": "https://i.imgur.com/JeI2LlW.jpg",
+      "group": [
+        "jj"
+      ]
+    },
+    {
+      "name": "松永里愛",
+      "img": "https://i.imgur.com/SXAqXPm.jpg",
+      "group": [
+        "jj"
+      ]
+    },
+    {
+      "name": "山岸理子",
+      "img": "https://i.imgur.com/F9vlQI5.jpg",
+      "group": [
+        "tf"
+      ]
+    },
+    {
+      "name": "新沼希空",
+      "img": "https://i.imgur.com/5bLkxpU.jpg",
+      "group": [
+        "tf"
+      ]
+    },
+    {
+      "name": "谷本安美",
+      "img": "https://i.imgur.com/AEvGux5.jpg",
+      "group": [
+        "tf"
+      ]
+    },
+    {
+      "name": "岸本ゆめの",
+      "img": "https://i.imgur.com/ldsT2TB.jpg",
+      "group": [
+        "tf"
+      ]
+    },
+    {
+      "name": "浅倉樹々",
+      "img": "https://i.imgur.com/sOttxv1.jpg",
+      "group": [
+        "tf"
+      ]
+    },
+    {
+      "name": "小野瑞歩",
+      "img": "https://i.imgur.com/izeGovR.jpg",
+      "group": [
+        "tf"
+      ]
+    },
+    {
+      "name": "小野田紗栞",
+      "img": "https://i.imgur.com/bhB141y.jpg",
+      "group": [
+        "tf"
+      ]
+    },
+    {
+      "name": "秋山眞緒",
+      "img": "https://i.imgur.com/q6iwPiD.jpg",
+      "group": [
+        "tf"
+      ]
+    },
+    {
+      "name": "一岡伶奈",
+      "img": "https://i.imgur.com/VQt2NZJ.jpg",
+      "group": [
+        "by"
+      ]
+    },
+    {
+      "name": "島倉りか",
+      "img": "https://i.imgur.com/eAck9di.jpg",
+      "group": [
+        "by"
+      ]
+    },
+    {
+      "name": "西田汐里",
+      "img": "https://i.imgur.com/P5gxs3q.jpg",
+      "group": [
+        "by"
+      ]
+    },
+    {
+      "name": "江口紗耶",
+      "img": "https://i.imgur.com/M3wCFKd.jpg",
+      "group": [
+        "by"
+      ]
+    },
+    {
+      "name": "高瀬くるみ",
+      "img": "https://i.imgur.com/R61bkMx.jpg",
+      "group": [
+        "by"
+      ]
+    },
+    {
+      "name": "前田こころ",
+      "img": "https://i.imgur.com/QyC922S.jpg",
+      "group": [
+        "by"
+      ]
+    },
+    {
+      "name": "山﨑夢羽",
+      "img": "https://i.imgur.com/4OMvdHK.jpg",
+      "group": [
+        "by"
+      ]
+    },
+    {
+      "name": "岡村美波",
+      "img": "https://i.imgur.com/KyZEtHF.jpg",
+      "group": [
+        "by"
+      ]
+    },
+    {
+      "name": "清野桃々姫",
+      "img": "https://i.imgur.com/MrTDh9L.jpg",
+      "group": [
+        "by"
+      ]
+    },
+    {
+      "name": "平井美葉",
+      "img": "https://i.imgur.com/YLyBTec.jpg",
+      "group": [
+        "by"
+      ]
+    },
+    {
+      "name": "小林萌花",
+      "img": "https://i.imgur.com/30vSgu5.jpg",
+      "group": [
+        "by"
+      ]
+    },
+    {
+      "name": "里吉うたの",
+      "img": "https://i.imgur.com/xpjH0nb.jpg",
+      "group": [
+        "by"
+      ]
+    },
+    {
+      "name": "米村姫良々",
+      "img": "https://i.imgur.com/DwqeeXA.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "中山夏月姫",
+      "img": "https://i.imgur.com/gvpLGbI.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "窪田七海",
+      "img": "https://i.imgur.com/YIT2U5z.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "松原ユリヤ",
+      "img": "https://i.imgur.com/GRXXkyS.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "斉藤円香",
+      "img": "https://i.imgur.com/rf0adCZ.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "小野田華凜",
+      "img": "https://i.imgur.com/R6HYHhe.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "広本瑠璃",
+      "img": "https://i.imgur.com/VtFjnmC.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "橋田歩果",
+      "img": "https://i.imgur.com/48FD515.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "西﨑美空",
+      "img": "https://i.imgur.com/HoFb7mx.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "平山遊季",
+      "img": "https://i.imgur.com/WdubJIX.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "北原もも",
+      "img": "https://i.imgur.com/KW92qrr.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "江端妃咲",
+      "img": "https://i.imgur.com/5y5ZV4t.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "豫風瑠乃",
+      "img": "https://i.imgur.com/CSnsBbm.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "村越彩菜",
+      "img": "https://i.imgur.com/JwQCYKQ.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "植村葉純",
+      "img": "https://i.imgur.com/3qi2OTC.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "有澤一華",
+      "img": "https://i.imgur.com/fdxcpDJ.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "石山咲良",
+      "img": "https://i.imgur.com/O7nYYjI.jpg",
+      "group": [
+        "kss"
+      ]
+    },
+    {
+      "name": "石栗奏美",
+      "img": "https://i.imgur.com/YAPe04i.jpg",
+      "group": [
+        "kss"
+      ]
+    }
+  ]
